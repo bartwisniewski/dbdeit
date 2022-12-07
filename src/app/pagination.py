9f294d32ec326cd39f1d17e7459d727f50c3a118
bartwisniewski <bartekwisnia@op.pl>
2022-12-07 22:38:53 +0100

    pagination refactoring, nested schema passed through constructor of pagination

--- src/app/pagination.py
+++ src/app/pagination.py
@@ -1,31 +1,38 @@
 from flask_marshmallow import fields
 from flask import Response
 from . import ma
+from .schemas import BlogEntrySchema
+
+
+class PaginatedObject:
+    def __init__(self, data, url, next, previous, total):
+        self.data = data
+        self.url = url
+        self.next = next
+        self.previous = previous
+        self.total = total
+
+
+class PaginatedSchema(ma.Schema):
+    url = fields.fields.Url()
+    next = fields.fields.Integer()
+    previous = fields.fields.Integer()
+    total = fields.fields.Integer()
+
+    def __init__(self, nested_schema, *args, **kwargs):
+        self._declared_fields['data'] = fields.fields.List(fields.fields.Nested(nested_schema()))
+        super().__init__(*args, **kwargs)
 
 
 class Pagination:
 
-    class PaginatedObject:
-        def __init__(self, data, url, next, previous, total):
-            self.data = data
-            self.url = url
-            self.next = next
-            self.previous = previous
-            self.total = total
-
     def __init__(self, nested_schema, limit=10, page=0):
-        class PaginatedSchema(ma.Schema):
-            url = fields.fields.Url()
-            next = fields.fields.Integer()
-            previous = fields.fields.Integer()
-            total = fields.fields.Integer()
-            data = fields.fields.List(fields.fields.Nested(nested_schema()))
         self.limit = limit
         self.page = page
         self.paginated_object = None
         self.queryset = None
         self.total = None
-        self.schema = PaginatedSchema()
+        self.schema = PaginatedSchema(nested_schema)
 
     def paginate_queryset(self, queryset, page: int):
         self.total = queryset.count()
@@ -64,5 +71,5 @@ class Pagination:
         previous = self.get_prev_page()
         total = self.total
         data = db.session.scalars(self.queryset).all()
-        self.paginated_object = Pagination.PaginatedObject(data, base_url, next, previous, total)
+        self.paginated_object = PaginatedObject(data, base_url, next, previous, total)
         return self.schema.jsonify(self.paginated_object)
